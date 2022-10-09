5ee112fffae6e36d733f79318c39c84376af4668
mafiageek <anton_kho@msn.com>
2022-10-09 14:01:48 +0800

    added search without function

---
+++ src/Search.js
@@ -0,0 +1,10 @@
+import { Input } from "reactstrap";
+import "./Search.css";
+
+export default function Search() {
+  return (
+    <div className="search">
+      <Input type="text" placeholder="Search"></Input>
+    </div>
+  );
+}
